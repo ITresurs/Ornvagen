63b81684a113925ac05f30ea1333c88e20bef1d0
itresurs <itresurslund@gmail.com>
2016-10-07 15:22:37 +0200

    start of using git

---
+++ NfcEditor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,361 @@
+﻿//using System;
+//using System.ComponentModel;
+//using System.Diagnostics;
+//using System.Runtime.CompilerServices;
+//using System.Windows.Input;
+//using Windows.Networking.Proximity;
+//using Windows.Storage.Streams;
+
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
+//using System.IO;
+//using System.Collections;
+
+//namespace NfcEditor.ViewModels
+//{
+//    class MainWindowViewModel : INotifyPropertyChanged
+//    {
+//        private string _Url;
+//        private bool _NfcDetected;
+//        private ProximityDevice _proximityDevice;
+//        private long _MessageType;
+//        string MyPath = System.IO.Path.Combine(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyMusic), "Ny mapp"), "ytterligare");
+//        string MyPath2 = System.IO.Path.Combine(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyMusic), "Ny mapp"), "ytterligareIgen");
+
+
+
+
+
+
+
+
+//        //public RelayCommand Play { get; set; }
+
+//        //private MediaElement media;
+
+//        //// Bound media property with notification
+//        //public MediaElement Media
+//        //{
+//        //    get
+//        //    {
+//        //        return this.media;
+//        //    }
+//        //    set
+//        //    {
+//        //        this.media = value;
+
+//        //        // Modify following line as needed
+//        //        // to any custom Property changed handler
+//        //        //this.Changed("Media");
+//        //    }
+//        //}
+
+
+
+
+
+
+
+
+
+//        public MainWindowViewModel()
+//        {
+//            _proximityDevice = Windows.Networking.Proximity.ProximityDevice.GetDefault();
+//            if (_proximityDevice != null)
+//            {
+//                _proximityDevice.DeviceArrived += _proximityDevice_DeviceArrived;
+//                _proximityDevice.DeviceDeparted += _proximityDevice_DeviceDeparted;
+//                _MessageType = _proximityDevice.SubscribeForMessage("WindowsUri", MessageReceivedHandler);
+//            }
+//            Console.WriteLine("hÄR ÄR litetext2FGHJKJHBGHJGYU(YTGYGTHYG");
+//            try
+//            {
+//                Directory.CreateDirectory(MyPath);
+//                Directory.CreateDirectory(MyPath2);
+//            }
+//            catch (Exception e)
+//            {
+//                Console.WriteLine("Failed: {0}", e.ToString());
+//            }
+//            finally { }
+
+
+
+//           // this.Play = new RelayCommand(this.ExecutePlayCommand);
+
+//            //// The Media element
+//            //this.Media = new MediaElement();
+
+//            //// This is where we need to set Loaded and Unloaded behavior to Manual
+//            //// This means video won't play right-away after loading
+//            //// You will have to call .Play() method to start playback
+//            //// Add a play button if you don't have one and bind the Play command
+//            //this.Media.LoadedBehavior = MediaState.Manual;
+//            //this.Media.UnloadedBehavior = MediaState.Manual;
+
+//            //// Following is just an example. Doesn't need to be here
+//            //this.Media.Source = new Uri("test.mp4", UriKind.Relative);
+
+
+
+
+
+
+//        }
+
+
+
+
+
+
+
+
+//        //public void ExecutePlayCommand()
+//        //{
+//        //    this.Media.Play();
+//        //}
+
+
+
+
+
+
+
+
+
+
+
+
+//        void _proximityDevice_DeviceDeparted(ProximityDevice sender)
+//        {
+//            NfcDetected = false;
+//            Url = "http://";
+//            Console.WriteLine("Här skall det ha oregistrerats");
+//        }
+
+//        void _proximityDevice_DeviceArrived(ProximityDevice sender)
+//        {
+//            NfcDetected = true;
+//            Console.WriteLine("Här skall det ha registrerats");
+//            SetMedia();
+//            //MediaElement MediaElementObject = new MediaElement();
+//           // MediaElementObject.Source = new Uri("C:\\Users\\001272\\Music\\Ny mapp\\wer.mp3");
+//           // MediaElementObject.Play();
+//            //PlayMedia();
+//            //PlayMedia("System.Windows.Controls.MediaElement");
+//            //MyMediaElement.Source = VideoToPlay;
+//            //MyMediaElement.Play();
+//        }
+
+//        private void MessageReceivedHandler(ProximityDevice sender, ProximityMessage message)
+//        {
+//            try
+//            {
+//                using (var reader = DataReader.FromBuffer(message.Data))
+//                {
+//                    reader.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf16LE;
+//                    string receivedString = reader.ReadString(reader.UnconsumedBufferLength / 2 - 1);
+//                    Debug.WriteLine("Received message from NFC: " + receivedString);
+//                    Url = receivedString;
+//                }
+
+//            }
+//            catch (Exception e)
+//            {
+//                Debug.WriteLine(e.StackTrace);
+//            }
+
+//        }
+
+
+//        private void DoWriteTag()
+//        {
+//            try
+//            {
+//                using (var writer = new DataWriter { UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf16LE })
+//                {
+//                    Debug.WriteLine("Writing message to NFC: " + Url);
+//                    writer.WriteString(Url);
+//                    long id = _proximityDevice.PublishBinaryMessage("WindowsUri:WriteTag", writer.DetachBuffer());
+//                    _proximityDevice.StopPublishingMessage(id);
+//                }
+//            }
+//            catch (Exception e)
+//            {
+//                Debug.WriteLine(e.StackTrace);
+//            }
+
+//        }
+
+//        RelayCommand _writeCommand;
+//        public ICommand WriteCommand
+//        {
+//            get
+//            {
+//                if (_writeCommand == null)
+//                {
+//                    _writeCommand = new RelayCommand(p => this.DoWriteTag(), p => this.NfcDetected);
+//                }
+//                return _writeCommand;
+//            }
+//        }
+
+//        public string Url
+//        {
+//            get { return _Url; }
+//            set
+//            {
+//                if (value.Equals(_Url)) return;
+//                OnPropertyChanged();
+//                _Url = value;
+//            }
+//        }
+
+//        public bool NfcDetected
+//        {
+//            get { return _NfcDetected; }
+//            set
+//            {
+//                _NfcDetected = value;
+//                OnPropertyChanged("NfcDetected");
+//                OnPropertyChanged("NfcSearching");
+//            }
+//        }
+
+//        public bool NfcSearching
+//        {
+//            get { return !_NfcDetected; }
+//        }
+
+
+
+//        public event PropertyChangedEventHandler PropertyChanged;
+
+//        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+//        {
+//            PropertyChangedEventHandler handler = PropertyChanged;
+//            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+//        }
+
+
+
+//        //public void Element_MediaElementEnded(object sender, RoutedEventArgs e)
+//        //{
+//        //    NfcEditor.Views.MainWindow.MediaElement.Stop(); //Stops and resets media to be played again
+//        //}
+
+//        //Eventhanterare för det fall mediefilen inte kan läsas.
+//        private void MediaElement_MediaFailed(object sender, ExceptionRoutedEventArgs e)
+//        {
+//            Console.WriteLine();
+//            Console.WriteLine("GetFilderPath: {0}");//,
+//            //filen2());
+//        }
+//        //public static void Mede()
+//        //{
+//        //    Console.WriteLine();
+//        //    Console.WriteLine("GetFilderPath: {0}");//,
+//        //    //filen2());
+//        //}
+
+//        void SetMedia()
+//        {
+//            var somefiles = Directory.EnumerateFiles(
+//                MyPath, "*.*", SearchOption.AllDirectories);
+//            if (somefiles.Count() != 0)
+//            {
+//                VideoToPlay = new Uri(somefiles.ElementAt(new Random().Next(0, somefiles.Count())));
+//                //MediaElement.Source = new System.Uri(file);
+//                return;
+//            }
+//            else return;
+
+//        }
+
+
+
+
+
+        
+//        //Stuff for mediaelement, hopfully.
+//        //private MediaElement MyMediaElement;
+
+//        private Uri _videoToPlay;
+
+//        public Uri VideoToPlay
+//        {
+//            get { return _videoToPlay; }
+//            set
+//            {
+//                _videoToPlay = value;
+//                OnPropertyChanged("VideoToPlay");
+//            }
+//        }
+
+//        //RelayCommand _playMediaCommand;
+//        //public ICommand PlayMediaCommand
+//        //{
+//        //    get
+//        //    {
+//        //        if (_playMediaCommand == null)
+//        //        {
+//        //            _playMediaCommand = new RelayCommand(p => PlayMedia(p),
+//        //                p => true);
+//        //        }
+//        //        Console.WriteLine("gabbagabba: {0}", _playMediaCommand);
+//        //        return _playMediaCommand;
+//        //    }
+//        //}
+
+//        //void PlayMedia(object param)
+//        //{
+//        //    Console.WriteLine("detta är PlayMedia() som försöker skriva ut _playmediacommand: {0}", param);
+//        //    var paramMediaElement = (MediaElement)param;
+//        //    MyMediaElement = paramMediaElement;
+//        //    MyMediaElement.Source = VideoToPlay;
+//        //    MyMediaElement.Play();
+//        //}
+
+
+//        //protected void OnPropertyChanged(string propertyname)
+//        //{
+//        //    var handler = PropertyChanged;
+//        //    if (handler != null)
+//        //        handler(this, new PropertyChangedEventArgs(propertyname));
+//        //}
+
+//        //public event PropertyChangedEventHandler PropertyChanged;
+//        //public event PropertyChangedEventHandler PropertyChanged;
+
+//        //private MediaElement _mediaElementObject;
+
+//        //public MediaElement MediaElementObject
+//        //{
+//        //    get { return _mediaElementObject; }
+//        //    set { _mediaElementObject = value; RaisePropertyChanged("MediaElementObject"); }
+//        //}
+
+
+//        private void RaisePropertyChanged(string propertyName)
+//        {
+//            // take a copy to prevent thread issues
+//            PropertyChangedEventHandler handler = PropertyChanged;
+//            if (handler != null)
+//            {
+//                handler(this, new PropertyChangedEventArgs(propertyName));
+//            }
+//        }
+//   // MediaElement MediaElementObject = new MediaElement();
+
+//    }
+//}
---
+++ NfcEditor/Views/MainWindow.xaml.cs
@@ -0,0 +1,296 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web.Script.Serialization;
+using System.Windows;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+using Windows.Networking.Proximity;
+using Windows.Storage.Streams;
+
+namespace Ornvagen.Views
+{
+
+    public partial class MainWindow : Window
+    {
+        private bool _NfcDetected;
+        private ProximityDevice _proximityDevice;
+        private long _MessageType;
+
+        adresslista AdressListan = new adresslista();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            AdressListan.readdictJson();
+
+            _proximityDevice = Windows.Networking.Proximity.ProximityDevice.GetDefault();
+            if (_proximityDevice != null)
+            {
+                _MessageType = _proximityDevice.SubscribeForMessage("WindowsUri", MessageReceivedHandler);
+                _proximityDevice.DeviceArrived += _proximityDevice_DeviceArrived;
+                _proximityDevice.DeviceDeparted += _proximityDevice_DeviceDeparted;
+            }
+        }
+
+
+        void _proximityDevice_DeviceDeparted(ProximityDevice sender)
+        {
+            NfcDetected = false;
+            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() => { showing(); AdressListan.LastCode = null; }));
+            Debug.WriteLine("kul, nu blev det null. Det här är lastcode nu: ---"+ AdressListan.LastCode +"---   ---------------------------------------------");
+        }
+
+        void _proximityDevice_DeviceArrived(ProximityDevice sender)
+        {
+            NfcDetected = true;
+            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() => { showing(); }));
+            Debug.WriteLine("Det här är lascode när vår tagg uppmärksammas i proximitydevice arives: "+AdressListan.LastCode+" se där! --------------------------------------------------------");
+        }
+
+        private void MessageReceivedHandler(ProximityDevice sender, ProximityMessage message)
+        {           
+            try
+            {
+                using (var reader = DataReader.FromBuffer(message.Data))
+                {
+                    reader.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf16LE;
+                    string receivedString = reader.ReadString(reader.UnconsumedBufferLength / 2 - 1);
+                    Debug.WriteLine("Received message from NFC: " + receivedString);
+                    //Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() => { winBorder(6); }));'
+                    Debug.WriteLine("det här är lastcode innan den ändras av inkommande kod: "+AdressListan.LastCode+" ta da!------------------------------------------------");
+                    Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() => { winBorder(6); navigera(AdressListan.varde(receivedString)); AdressListan.LastCode = receivedString; }));
+                    //Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() => { AdressListan.LastCode = receivedString; }));
+                }
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.StackTrace);
+            }
+         }
+
+        private void DoWriteTag(string adress)
+        {
+            try
+            {
+                using (var writer = new DataWriter { UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf16LE })
+                {
+                    Debug.WriteLine("Writing message to NFC: " + adress);
+                    writer.WriteString(adress);
+                    long id = _proximityDevice.PublishBinaryMessage("WindowsUri:WriteTag", writer.DetachBuffer());
+                    _proximityDevice.StopPublishingMessage(id);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.StackTrace);
+            }
+        }
+
+        public bool NfcDetected
+        {
+            get { return _NfcDetected; }
+            set
+            {
+                _NfcDetected = value;
+                OnPropertyChanged("NfcDetected");
+                OnPropertyChanged("NfcSearching");
+            }
+        }
+
+        public bool NfcSearching
+        {
+            get { return !_NfcDetected; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void winBorder(int x) 
+        {
+            Thickness ram = window.BorderThickness;
+            ram.Bottom = x;
+            ram.Left = x;
+            ram.Right = x;
+            ram.Top = x;
+            window.BorderThickness = ram;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {      
+            if(_NfcDetected)
+            {
+                string keyCheck = AdressListan.LastCode;
+                Debug.WriteLine("i knapptryck, Adresslistan.lastcode = "+ AdressListan.LastCode +" och keycheck = "+ keyCheck +"---------------------------------------------------------");
+                if (AdressListan.exists(keyCheck))
+                {
+                    AdressListan.remove(keyCheck);
+                    AdressListan.adding(keyCheck, adressNu().ToString());
+                    AdressListan.writedictJson();
+                    showingClosed();
+                }
+                else
+                {
+                    String dat = DateTime.Now.ToString();
+                    DoWriteTag(dat);
+                    AdressListan.adding(dat, adressNu().ToString());
+                    AdressListan.writedictJson();
+                    showing();
+                }
+            }
+        }
+
+        private void Other_Button_Click(object sender, RoutedEventArgs e)
+        {
+            navigera("about:blank");
+
+            Thickness marginal = webBrowser.Margin;
+            marginal.Top = -200;
+            webBrowser.Margin = marginal;
+
+            Thickness margin = HELLOHELLO_.Margin;
+            margin.Bottom = 0;
+            HELLOHELLO_.Margin = margin;
+
+        }
+
+        public void navigera(string hit)
+        {
+                webBrowser.Navigate(hit);     
+        }
+
+        public Uri adressNu()
+        {
+            return webBrowser.Source;
+        }
+
+        public void showing()
+        {
+                if (_NfcDetected)
+            {
+                knapp.IsEnabled = true;
+            }
+            else
+            {
+                knapp.IsEnabled = false;
+            }
+        }
+
+        public void showingClosed()
+        {
+                knapp.IsEnabled = false;
+        }
+
+        private void webbLoaded(object sender, NavigationEventArgs e)
+        {
+            winBorder(0);
+            Debug.WriteLine("sparade följande: " + AdressListan.LastCode);
+        }
+    }
+
+    public class adresslista
+    {
+
+        Dictionary<string, string> tags
+                = new Dictionary<string, string>{};
+
+        private string lastCode;
+        public string LastCode { get { return lastCode; } set { lastCode = value; } }
+        private string goTo = "http://accessyoutube.org.uk/";
+        public string GoTo { get { return goTo; } set { goTo = value; } }
+
+        string filpath = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + @"\NFC.dat";
+
+        public Boolean exists(string x) 
+        {
+            Debug.WriteLine("i exists---------------------------------------------------------");
+                if (x != null && tags.ContainsKey(x))
+                {
+                    Debug.WriteLine("levererar true i exists---------------------------------------------------------");
+                    return true;
+                }
+            else
+            {
+                Debug.WriteLine("leverrerar false i exists---------------------------------------------------------");
+                return false;
+            }
+        }
+
+        public string varde(string text)
+        {
+            string goHit = null;
+
+            if (text !=null && tags.TryGetValue(text, out goHit) && goHit != null)
+            {
+                
+            //if (tags.TryGetValue(text, out goHit))
+            //{
+            //    if (!goHit.Equals(null))
+            //    {
+                    return goHit;
+            //    }
+            //    else return goTo;
+            //}
+            //else return goTo;
+            }
+            else return goTo;
+        }
+
+        public void remove(string key) 
+        {
+            tags.Remove(key);
+        }
+
+        public void adding(string key, string value) 
+        {
+            tags.Add(key, value);
+        }
+
+        public void readdictJson()
+        {
+            if (File.Exists(filpath))
+            {
+                try
+                {
+                    var dictionary = new JavaScriptSerializer()
+    .Deserialize<Dictionary<string, string>>(File.ReadAllText(filpath));
+                    
+                    Debug.WriteLine("antal i dict" + dictionary.Count());
+                    if (dictionary.Count() > 0)
+                    {
+                        tags = dictionary;
+                    }
+                    
+                }
+                catch (Exception)
+                {
+                    Debug.WriteLine("Fel på läsningen");
+                }
+            }
+            else File.Create(filpath);
+        }
+
+        public void writedictJson()
+        {
+            try
+            {
+                File.WriteAllText(filpath, new JavaScriptSerializer().Serialize(tags));
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine("fel på skrivningen");
+            }
+        }
+    }
+}
